31abad756e7cb16152b462d41e7c562c7876ee14
Naeal Hasan <naealhasan.dev@gmail.com>
2026-05-07 13:12:13 +0600

    add getMinValue function

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 console.log("vs code is Awesome...");
+const numbers = [1, 2, 3, 7, 4, -50];
+
+const getMinValue = (arr) => {
+   return arr.reduce(
+      (min, current) => (min <= current ? min : current),
+      Infinity,
+   );
+};
